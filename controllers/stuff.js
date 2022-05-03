b88924d143c105dbe4009bc94202df88a7441d5c
fnedelec <fnedelec@ecoceatechnologies.com>
2022-05-03 16:12:56 +0200

    fix security when deleting a thing

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -64,19 +64,33 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
+    Thing.findOne({ _id: req.params.id }).then(
+        (thing) => {
+            if (!thing) {
+                res.status(404).json({
+                    error: new Error('No such Thing!')
+                });
+            }
+            if (thing.userId !== req.auth.userId) {
+                res.status(400).json({
+                    error: new Error('Unauthorized request!')
+                });
+            }
+            Thing.deleteOne({ _id: req.params.id }).then(
+                () => {
+                    res.status(200).json({
+                        message: 'Deleted!'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(400).json({
+                        error: error
+                    });
+                }
+            );
         }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
+    )
 };
 
 exports.getAllStuff = (req, res, next) => {
